ce365dfa6907bbc37a852ccb63938d649a39c217
Sribarathvajasarma <62195656+Sribarathvajasarma@users.noreply.github.com>
2021-08-21 20:29:15 +0530

    Create File Structure and Catalog, Circulation Feature
    *Add controller and views
    
    * Add files via upload
    
    Co-authored-by: AntonSinthujan <84249184+AntonSinthujan@users.noreply.github.com>

---
+++ circulation_module/Public/index.php
@@ -0,0 +1,14 @@
+<?php
+define('ROOT',dirname(__DIR__). DIRECTORY_SEPARATOR);
+define('APP', ROOT. 'App'. DIRECTORY_SEPARATOR);
+define('VIEW', ROOT. 'App'. DIRECTORY_SEPARATOR. 'View' . DIRECTORY_SEPARATOR);
+define('MODEL', ROOT. 'App'. DIRECTORY_SEPARATOR. 'Model' . DIRECTORY_SEPARATOR);
+define('DATA', ROOT. 'App'. DIRECTORY_SEPARATOR. 'Data' . DIRECTORY_SEPARATOR);
+define('CORE', ROOT. 'App'. DIRECTORY_SEPARATOR. 'Core' . DIRECTORY_SEPARATOR);
+define('CONTROLLER', ROOT. 'App'. DIRECTORY_SEPARATOR. 'Controller' . DIRECTORY_SEPARATOR);
+$modules =[ROOT,APP,CORE,CONTROLLER,DATA];
+
+set_include_path(get_include_path().PATH_SEPARATOR . implode(PATH_SEPARATOR,$modules));
+spl_autoload_register('spl_autoload',false);
+
+new Application;
