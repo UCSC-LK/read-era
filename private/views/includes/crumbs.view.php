d38c990b406a9cbfcb9e97a2c33a0601cd6de8a3
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2021-10-12 20:45:08 +0530

    this commit remove private folder

--- private/views/includes/crumbs.view.php
+++
@@ -1,9 +0,0 @@
-<nav>
-  <ol>
-    <?php if(isset($crumbs)):?>
-      <?php foreach ($crumbs as $crumb):?>
-         <li><a href="<?=$crumb[1]?>"><?=$crumb[0]?></a></li>
-     <?php endforeach;?>
-    <?php endif;?>
-  </ol>
-</nav>
