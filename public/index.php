888a24538c29dc655864582192daa015c2cce3e6
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2021-10-12 21:01:18 +0530

    this commit resolves the conflicts happen to index.css and index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,6 @@
 <?php
 session_start();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-require("../private/core/autoload.php");
-=======
 require("../app/core/autoload.php");
->>>>>>> reservation-try
-=======
-require("../app/core/autoload.php");
-=======
-require("../private/core/autoload.php");
->>>>>>> forgot-try
->>>>>>> reservation-try
 
 $app = new App();
