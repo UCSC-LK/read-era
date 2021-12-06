10b3c779810af1531776bd3f0029018dbd04e7ef
Rumesha Disanayaka <rumesha2chathumini@gmail.com>
2021-12-06 10:38:26 +0530

    Modified New Report Controller

--- app/controllers/Reports.php
+++ app/controllers/Reports.php
@@ -81,22 +81,14 @@ Class Reports extends Controller
             $data1 = $data;
             
         }
-
-            
-
         }
         
         if(!isset($data))
         {
             $this->redirect("reports");
         }
-     
-        
-
-
         $crumbs[] = ['Dashboard', ''];
         $crumbs[] = ['Reports', 'reports'];
-
 
         $this->view('reports.generate', [
             'rows' => $data,
@@ -176,7 +168,154 @@ Class Reports extends Controller
             'crumbs' => $crumbs,
         ]);
     }
+    public function withdraw()
+    {
+        if (!Auth::logged_in()) {
+            $this->redirect('landing');
+        }
+        $lost = new Catalog();
+        $query = "select * from catalogs where status = 'withdrawn'";
+        $data = $lost->query($query);
 
 
+        $crumbs[] = ['Dashboard', ''];
+        $crumbs[] = ['Reports', 'reports'];
 
+
+        $this->view('reports.withdraw', [
+            'rows' => $data,
+            'crumbs' => $crumbs,
+        ]);
+    }
+    public function generatew()
+    {
+        if (!Auth::logged_in()) {
+            $this->redirect('landing');
+        }
+
+        $lost = new Catalog();
+        $query = "select * from catalogs where status = 'withdrawn'";
+        $data = $lost->query($query);
+
+        $crumbs[] = ['Dashboard', ''];
+        $crumbs[] = ['Reports', 'reports'];
+
+        $this->view('reports.generatew', [
+            'rows' => $data,
+            'crumbs' => $crumbs,
+        ]);
+    }
+
+    public function inventory()
+    {
+        if (!Auth::logged_in()) {
+            $this->redirect('landing');
+        }
+        $books = new Catalog();
+        $query = "select * from catalogs";
+        $data = $books->query($query);
+
+        $arr = array();
+
+        $data1 = $books->query("select count(id) as count from catalogs");
+        $data1 = $data1[0];
+        $data1 = $data1->count;
+        $arr[0] = $data1;
+
+        $data2 = $books->query("select count(id) as count from catalogs where status='available'");
+        $data2 = $data2[0];
+        $data2 = $data2->count;
+        $arr[1] = $data2;
+
+        $data3 = $books->query("select count(id) as count from catalogs where status='lost' ");
+        $data3 = $data3[0];
+        $data3 = $data3->count;
+        $arr[2] = $data3;
+
+        $data4 = $books->query("select count(id) as count from catalogs where status='damage'");
+        $data4 = $data4[0];
+        $data4 = $data4->count;
+        $arr[3] = $data4;
+
+        $data5 = $books->query("select count(id) as count from catalogs where status='borrowed'");
+        $data5 = $data5[0];
+        $data5 = $data5->count;
+        $arr[4] = $data5;
+
+        $data6 = $books->query("select count(id) as count from catalogs where status='reserved'");
+        $data6 = $data6[0];
+        $data6 = $data6->count;
+        $arr[5] = $data6;
+
+        $data7 = $books->query("select count(id) as count from catalogs where status='withdrawn'");
+        $data7 = $data7[0];
+        $data7 = $data7->count;
+        $arr[6] = $data7;
+
+
+        $crumbs[] = ['Dashboard', ''];
+        $crumbs[] = ['Reports', 'reports'];
+
+
+        $this->view('reports.inventory', [
+            'rows' => $data,
+            'crumbs' => $crumbs,
+            'arr'=>$arr,
+        ]);
+    }
+    public function generatei()
+    {
+        if (!Auth::logged_in()) {
+            $this->redirect('landing');
+        }
+        $books = new Catalog();
+        $query = "select * from catalogs";
+        $data = $books->query($query);
+
+        $arr = array();
+
+        $data1 = $books->query("select count(id) as count from catalogs");
+        $data1 = $data1[0];
+        $data1 = $data1->count;
+        $arr[0] = $data1;
+
+        $data2 = $books->query("select count(id) as count from catalogs where status='available'");
+        $data2 = $data2[0];
+        $data2 = $data2->count;
+        $arr[1] = $data2;
+
+        $data3 = $books->query("select count(id) as count from catalogs where status='lost' ");
+        $data3 = $data3[0];
+        $data3 = $data3->count;
+        $arr[2] = $data3;
+
+        $data4 = $books->query("select count(id) as count from catalogs where status='damage'");
+        $data4 = $data4[0];
+        $data4 = $data4->count;
+        $arr[3] = $data4;
+
+        $data5 = $books->query("select count(id) as count from catalogs where status='borrowed'");
+        $data5 = $data5[0];
+        $data5 = $data5->count;
+        $arr[4] = $data5;
+
+        $data6 = $books->query("select count(id) as count from catalogs where status='reserved'");
+        $data6 = $data6[0];
+        $data6 = $data6->count;
+        $arr[5] = $data6;
+
+        $data7 = $books->query("select count(id) as count from catalogs where status='withdrawn'");
+        $data7 = $data7[0];
+        $data7 = $data7->count;
+        $arr[6] = $data7;
+
+        $crumbs[] = ['Dashboard', ''];
+        $crumbs[] = ['Reports', 'reports'];
+
+        $this->view('reports.generatei', [
+            'rows' => $data,
+            'crumbs' => $crumbs,
+            'arr'=>$arr,
+        ]);
+    }
 }
