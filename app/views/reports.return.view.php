61586ed10e3ac3a7b6c12486139e949fc7bec452
Rumesha Disanayaka <rumesha2chathumini@gmail.com>
2021-12-17 20:46:30 +0530

    Added Return Report

---
+++ app/views/reports.return.view.php
@@ -0,0 +1,46 @@
+<?php $this->view('includes/header')?>
+<?php $this->view('includes/nav')?>
+<?php $this->view('includes/topbar')?>
+
+<style>
+    .update_profile_button{
+        margin-right: 10px;
+    }
+    label{
+        margin-right: 20px;
+    }
+</style>
+
+
+<div class="home-content">
+    <div class="content-box">
+        <div class="box1 box">
+            <div class="header">
+                <div class="title">Returned Books</div>
+            </div>
+            <table class="table table-striped table-hover">
+                <tr><th>Title</th><th>Member</th><th>Issue Date</th><th>Deadline</th><th>Fine Amount</th>
+                </tr>
+                <?php if($rows):?>
+                    <?php foreach ($rows as $row):?>
+                        <tr><td><?=$row->book_id?></td><td><?=$row->member_id?></td><td><?=get_date($row->issue_date)?></td><td><?=get_date($row->deadline)?></td>
+                            <td><?=$row->fine?></td></tr>
+                    <?php endforeach;?>
+                <?php else:?>
+                    <h4>No return books were found at this time</h4>
+                <?php endif;?>
+            </table>
+            <a href="<?=ROOT?>/reports/generater" class="update_profile_button">Generate</a>
+            <a href="<?=ROOT?>/reports" class="update_profile_button">Back</a>
+        </div>
+    </div>
+</div>
+</section>
+<?php $this->view('includes/footer')?>
+
+
+
+
+
+
+
